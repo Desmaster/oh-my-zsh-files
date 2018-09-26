4c28d133b831ea028a49bc6bdd33932d48e3a95b
Timon de Groot <timon@marissen.net>
2018-09-26 13:54:22 +0200

    Add m2-create-admin.zsh

---
+++ m2-create-admin.zsh
@@ -0,0 +1,1 @@
+alias m2-create-admin='bin/magento admin:user:create --admin-user=$USER --admin-password=Test123$ --admin-email=$USER@$HOST.localhost --admin-firstname=$USER --admin-lastname="Admin"'
